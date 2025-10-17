50781533c9ddd33c273014644b7fe1ec7b41f01d
m_u_say@hotmail.com <m_u_say@hotmail.com>
2025-10-17 14:16:41 +0900

    client componentの最適化

---
+++ src/store/StoreProvider.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+type StoreProviderProps = {
+  children: ReactNode;
+};
+
+const StoreProvider = ({ children }: Readonly<StoreProviderProps>) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+export default StoreProvider;
